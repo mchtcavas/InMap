0781e5bccf6310208a72ab1c8a225531592b2165
Mücahit Çavaş <mchtcavas@gmail.com>
2024-08-08 11:39:19 +0300

    Initial project setup

---
+++ src/test/java/com/mchtcavas/InMap/InMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchtcavas.InMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
